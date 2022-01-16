0e7b10642e747cc0901f3f7917305f5a4213b4e8
Gavin <tianyaogavin@gmail.com>
2022-01-17 00:47:33 +0800

    add .gitignore

--- src/common/constants.ts
+++
@@ -1,6 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-export const HASURA_URL = process.env.HASURA_URL;
-export const HASURA_ADMIN_KEY = process.env.HASURA_ADMIN_KEY;
-export const STACKS_NODE_SQLITE_PATH = process.env.DATA_ROOT_PATH
